11f29f9b006414c3d142801d3f1140eaadc91084
2x2devcode <admin@2x2coin.com>
2026-09-01 15:41:40 -0300

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/infinitericks.ico
+ICON_DST=../../src/qt/res/icons/infinitericksico.ico
 
 convert ../../src/qt/res/icons/infinitericks-16.png ../../src/qt/res/icons/infinitericks-32.png ../../src/qt/res/icons/infinitericks-48.png ${ICON_DST}
